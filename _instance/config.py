71a2009b19bf0410f6b0c709785d59682a7649cf
Bersnard <bersnardc@gmail.com>
2018-12-12 17:16:19 -0500

    Instance refer

---
+++ _instance/config.py
@@ -0,0 +1,7 @@
+DEBUG = True
+SQLALCHEMY_ECHO = False
+ASSETS_DEBUG = True
+CACHE_TYPE = 'simple'
+CACHE_DEFAULT_TIMEOUT = 50
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/todolist_db'
